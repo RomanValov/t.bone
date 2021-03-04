421984462d492e119a0a9e5d459897bcf64283fd
Roman Valov <roman.valov@gmail.com>
2021-03-04 18:19:03 +0300

    ref: split main and bone

--- src/t/__init__.py
+++ src/t/__init__.py
@@ -27,7 +27,7 @@ def hint(name):
     raise NotImplementedError(name)
 
 
-def main(opts, argv, init):
+def bone(opts, argv, init):
     pipe = ' '.join(argv).split('/')
 
     data = None
@@ -48,45 +48,3 @@ def main(opts, argv, init):
             break
 
     return data
-
-
-
-def bone(sys):
-    import json
-    from collections import defaultdict
-
-    opts = defaultdict(bool)
-
-    argv = []
-    for item in sys.argv[1:]:
-        extra, _, value = item.partition('--')
-
-        if not extra and value:
-            opts[value] = True
-        else:
-            argv.append(item)
-
-    ilines = sys.stdin
-    olines = sys.stdout
-
-    if opts['jsoni'] or opts['json']:
-        init = lambda: json.loads(ilines.read())
-    else:
-        init = lambda: json.loads(ilines.read())
-
-    if opts['debug']:
-        print(opts, file=sys.stderr)
-        print(argv, file=sys.stderr)
-
-    data = main(opts, argv, init)
-
-    if opts['debug']:
-        print(type(data), file=sys.stderr)
-
-    text = str() if data is None else str(data)
-
-    if text:
-        olines.write(text)
-        olines.write('\n')
-
-    olines.flush()
